94dface2b9ffdb8b2932ec9f4136f8a3459dd1dc
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2024-01-10 14:09:32 -0800

    added in project 3

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,7 +11,7 @@ export default function Projects(){
     <div className="row mt-4">
         <Project title={"Scenery Finder"} image="project1.jpg" repo="https://github.com/eviehoang/scenery-finder" deploy="https://eviehoang.github.io/scenery-finder/"/>    
         <Project title={"MySequel Adventure"} image="project2.jpg" repo="https://github.com/taylasagerios/my-sequel-adventure" deploy="https://my-sequel-adventure-a9247b63bc9a.herokuapp.com/"/>
-        <Project title={"Project 3 coming soon"} />
+        <Project title={"White Chapel Mysteries"} image="project3.png" repo="https://github.com/Armand57araujo/whiteChapelMysteries" deploy="http://whitechapelmysteries-fcef7ed93e83.herokuapp.com/"/>
     </div>
   
     </>
